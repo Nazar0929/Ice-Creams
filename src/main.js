b9ae5811a9a7bb3a1af98ad47ef7591429ae49c5
Nazar0929 <166243366+Nazar0929@users.noreply.github.com>
2025-02-08 20:40:28 +0200

    push

--- src/main.js
+++ src/main.js
@@ -18,22 +18,46 @@
 
   (() => {
     const refs = {
-      openModalBtns: document.querySelectorAll("[data-modal-open]"), // выбираем ВСЕ кнопки
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+        openFirstModalBtns: document.querySelectorAll("[data-modal-open]"), // Кнопки открытия первой модалки
+        closeFirstModalBtn: document.querySelector("[data-modal-close]"),
+        firstModal: document.querySelector("[data-modal-first]"),
+
+        openSecondModalBtn: document.querySelector("[data-modal-open-second]"), // Кнопка "Next"
+        closeSecondModalBtn: document.querySelector("[data-modal-close-second]"),
+        secondModal: document.querySelector("[data-modal-second]"),
     };
-  
-    refs.openModalBtns.forEach(btn => btn.addEventListener("click", toggleModal)); // добавляем обработчик на каждую кнопку
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
+
+    // Открытие и закрытие первой модалки
+    refs.openFirstModalBtns.forEach(btn => btn.addEventListener("click", toggleFirstModal));
+    refs.closeFirstModalBtn.addEventListener("click", toggleFirstModal);
+    // Закрытие по клику на фон
+    refs.firstModal.addEventListener("click", (event) => {
+        if (event.target.classList.contains("backdrop")) toggleFirstModal();
+    });
+
+    function toggleFirstModal() {
+        refs.firstModal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
     }
-  })();
-  
 
+    // Открытие второй модалки без закрытия первой
+    refs.openSecondModalBtn.addEventListener("click", () => {
+        refs.secondModal.classList.remove("is-hidden");
+        document.body.classList.add("no-scroll");
+    });
 
+    // Закрытие второй модалки
+    refs.closeSecondModalBtn.addEventListener("click", toggleSecondModal);
+    // Закрытие по клику на фон
+    refs.secondModal.addEventListener("click", (event) => {
+        if (event.target.classList.contains("backdrop")) toggleSecondModal();
+    });
+
+    function toggleSecondModal() {
+        refs.secondModal.classList.add("is-hidden");
+        document.body.classList.remove("no-scroll");
+    }
+})();
 
 
 document.addEventListener("DOMContentLoaded", () => {
